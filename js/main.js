e7081deade1955171e9414cfbf267e7f68ca635d
volodymyr <drakeif@users.noreply.github.com>
2021-04-12 14:21:27 +0300

    v2.0

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5,
+    spaceBetween: 40,
+    freeMode: true,
+  });
+
+  var swiper = new Swiper('.swiper-container--lg', {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    freeMode: true,
+  });
